5d8aa200d8297b4186133970d282c3452b08b472
Erwann20 <erwann.bret@gmail.com>
2020-03-05 19:30:40 +0100

    init api-rest example

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//      /api/visiteur
+Route::namespace('Api')->group(function() {
+    Route::apiResource('visiteur', 'VisiteurAPIController');
+});
